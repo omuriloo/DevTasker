38f4e20e61c7cb6b9fb6fbd924156fa22fba3c2a
OttoMachado <158228292+OttoMachado@users.noreply.github.com>
2025-04-19 16:31:17 -0300

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Tasks } from "./Tasks";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ type: "varchar", length: 100, nullable: false})
+  @Column({ type: "varchar", length: 100, nullable: false })
   name: string;
 
-  @Column()
+  @Column({ type: "varchar", length: 100, nullable: false })
+  password: string;
+
+  @Column({ type: "varchar", length: 30, nullable: false })
+  creationDate: string;
+
+  @Column({ type: "varchar", length: 50, nullable: false })
   email: string;
 
-  constructor(name : string, email: string){
+  @OneToMany(() => Tasks, (task)  => task.user)   //OneToMany do Tasks pra User
+  tasks: Tasks[];
+
+  constructor(name : string, email: string, creationDate: string, password: string){
     this.name = name;
-    this.email = email
+    this.email = email;
+    this.creationDate = creationDate
+    this.password = password
   }
 }
